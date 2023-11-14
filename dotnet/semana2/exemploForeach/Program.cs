74d7b8c32407417988f6074aa527d31a643a9d79
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-14 15:16:32 -0300

    feat: usando o foreach para imprimir uma lista de compras

---
+++ dotnet/semana2/exemploForeach/Program.cs
@@ -0,0 +1,15 @@
+﻿// Tradução: “para cada…”, ou seja, realize algo para cada item de uma coleção/lista
+/*
+    pecorre do inicio ao fim de uma lista
+    string[] colecao= { "Item1", "Item2", "Item3", "Item4" };
+    foreach (string qualquerNome in colecao)
+    {
+        Console.WriteLine(qualquerNome);
+    }
+*/
+string[] mercado = {"Banana", "Maçã", "Arroz", "Uva", "Feijao"};
+Console.WriteLine("Lista de compras:");
+foreach (string item in mercado)
+{
+    Console.WriteLine(item);
+}
