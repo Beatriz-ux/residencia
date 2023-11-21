b7bdfaa8dafe53d79c8ad30153122bdf87f88c0c
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-20 21:44:30 -0300

    feat: finalizando P002

--- dotnet/semana2/P002/Program.cs
+++ dotnet/semana2/P002/Program.cs
@@ -1,59 +1,159 @@
-int opcao ;
-List<string> tarefas = new List<string>();
+﻿int opcao ;
+int id=1;
+int concluidas = 0;
+List<string[]> tarefas = new List<string[]>();
 do{
-    Console.WriteLine("1- Criar tarefa"+
-    "2- Listar tarefas"+
-    "3- Marcar tarefa concluida"+
-    "4- Listar tarefas concluidas"+
-    "5- Listar tarefas pendentes"+
-    "6- Excluir tarefa"+
-    "7- Encontrar tarefa"+
-    "8- Dashboard"+
+    Console.WriteLine("\n1- Criar tarefa\n"+
+    "2- Listar tarefas\n"+
+    "3- Marcar tarefa concluida\n"+
+    "4- Listar tarefas concluidas\n"+
+    "5- Listar tarefas pendentes\n"+
+    "6- Excluir tarefa\n"+
+    "7- Pesquisar tarefa\n"+
+    "8- Dashboard\n"+
     "0- Sair");
     opcao = int.Parse(Console.ReadLine());
 
     switch(opcao){
         case 1:
-            Console.WriteLine("Digite a tarefa");
-            string tarefa = Console.ReadLine();
-            Console.WriteLine("Digite a data de conclusão:");
-            DateTime data = DateTime.Parse(Console.ReadLine());
-            tarefa += $" - {data}";
+            Console.WriteLine("Digite o titulo da tarefa:");
+            string titulo = Console.ReadLine();
+            Console.WriteLine("Digite a descrição da tarefa:");
+            string descricao = Console.ReadLine();
+            Console.WriteLine("Digite a data de conclusão da tarefa: dd/MM/yyyy");
+            string data = Console.ReadLine();
+            string identificador = id.ToString("D4");
+            titulo = titulo.ToLower();
+            descricao = descricao.ToLower();
 
+            string[] tarefa = {identificador,titulo,descricao, data, "false"};
             tarefas.Add(tarefa);
-
-            Console.WriteLine("Tarefa adicionada com sucesso:"+tarefa);
+            id++;
             break;
-
         case 2:
-            Console.WriteLine("Listando tarefas");
-            int i = 0;
-            foreach (string item in tarefas)
-            {   
-                Console.WriteLine($"{i} - {item}");
+            Console.WriteLine($"{"id",-10} {"Titulo",-30} {"Descrição",-45} {"Data",-14}");
+            foreach(string[] t in tarefas){
+                if(t[4] == "false"){
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{t[0],-10} {t[1],-30} {t[2],-45} {t[3],-14}");
+                    Console.ResetColor();
+                }
+                else
+                    Console.WriteLine($"{t[0],-10} {t[1],-30} {t[2],-45} {t[3],-14}");
             }
             break;
-
         case 3:
-            Console.WriteLine("Digite o indice da tarefa");
-            int indice = int.Parse(Console.ReadLine());
-            tarefas.RemoveAt(indice);
-            Console.WriteLine("Tarefa removida com sucesso");
+            bool achouId = false;
+            Console.WriteLine("Digite o id da tarefa:");
+            string idTarefa = Console.ReadLine();
+            foreach(string[] t in tarefas){
+                if(t[0] == idTarefa ){
+                    if(t[4] == "true"){
+                        Console.WriteLine("Tarefa já concluida");
+                    }else{
+                        Console.WriteLine("Tarefa concluida!!!");
+                        t[4] = "true";
+                        concluidas++;
+                        
+                    }
+                    achouId = true;
+                    break;
+                    
+                }
+            }
+            if(!achouId)
+                Console.WriteLine("Tarefa não encontrada");
             break;
-        
         case 4:
-            Console.WriteLine("Listando tarefas concluidas");
-            foreach (string item in tarefas)
-            {   
-                if(item.Contains("concluida")){
-                    Console.WriteLine(item);
+            Console.WriteLine($"{"id",-10} {"Titulo",-30} {"Descrição",-45} {"Data",-14}");
+            foreach(string[] t in tarefas){
+                if(t[4] == "true"){
+                    Console.WriteLine($"{t[0],-10} {t[1],-30} {t[2],-45} {t[3],-14}");
                 }
             }
             break;
+        case 5:
+            Console.WriteLine($"{"id",-10} {"Titulo",-30} {"Descrição",-45} {"Data",-14}");
+            foreach(string[] t in tarefas){
+                if(t[4] == "false"){
+                    Console.WriteLine($"{t[0],-10} {t[1],-30} {t[2],-45} {t[3],-14}");
+                }
+            }
+            break;
+        case 6:
+            bool deletou = false;
+            Console.WriteLine("Digite o id da tarefa:");
+            string idTarefaExcluir = Console.ReadLine();
+            foreach(string[] t in tarefas){
+                if(t[0] == idTarefaExcluir){
+                    tarefas.Remove(t);
+                    if(t[4] == "true"){
+                        concluidas--;
+                        deletou = true;
+                        break;
+                    }
+                        
+                }
+            }
+            if(!deletou)
+                Console.WriteLine("Tarefa não encontrada");
+            break;
+        
+        case 7:
+            bool achou = false;
+            Console.WriteLine("Digite oque quer buscar:");
+            string buscarTarefa = Console.ReadLine();
+            buscarTarefa = buscarTarefa.ToLower();
+            Console.WriteLine("Resultado da busca:");
+            foreach(string[] t in tarefas){
+                if(t[0] == buscarTarefa || t[1].Contains(buscarTarefa) || t[2].Contains(buscarTarefa) || t[3].Contains(buscarTarefa)){
+                    Console.WriteLine($"{t[0],-10} {t[1],-30} {t[2],-45} {t[3],-14}");
+                    achou = true;
+                  
+                }
+            }
+            if(!achou){
+                Console.WriteLine("Tarefa não encontrada");
+            }
+            break;
+
+        case 8:
+            Console.WriteLine($"Dashboard");
+            Console.WriteLine("----------------------------------");
+            int pendentes = tarefas.Count - concluidas;
+            Console.Write("Seu progresso: ");
+            for(int i=0;i<pendentes;i++){
+                Console.Write("□ ");
+            }
+            for(int i=0;i<concluidas;i++){
+                Console.Write("■ ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"{"pendentes",-10} {"concluidas",-10}");
+            Console.WriteLine($"{pendentes,-10} {concluidas,-10}");
+            Console.WriteLine();
+            Console.WriteLine();
+            if(tarefas.Count > 0){
+                int posicao = encontraAntiga(tarefas);
+                Console.WriteLine($"Tarefa mais antiga: ({tarefas[posicao][3]})");
+                Console.WriteLine(tarefas[posicao][0]+" "+tarefas[posicao][1]);
 
 
+                posicao = encontraRecente(tarefas);
+                Console.WriteLine($"Tarefa mais recente: ({tarefas[posicao][3]})");
+                Console.WriteLine(tarefas[posicao][0]+" "+tarefas[posicao][1]);
+            }
+            
+            break;
 
 
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
 }
 while(opcao  !=0);
@@ -82,3 +182,4 @@ static int encontraRecente(List<string[]> tarefas){
     }
     return recente;
 }
+
