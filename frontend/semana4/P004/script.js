6d74ea99da376353f988b8c7a468ec0c8a522816
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-07 00:19:54 -0300

    feat:  função para printar beneficio já que é um objeto

---
+++ frontend/semana4/P004/script.js
@@ -0,0 +1,26 @@
+function printBeneficios(beneficios){
+  let beneficio = "";
+
+  if(beneficios.cafe){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Café da manhã</li>";
+  }
+  if(beneficios.ida_e_volta){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Aéreo ida e volta</li>";
+  }
+  if(beneficios.almoco){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Almoço</li>";
+  }
+  if(beneficios.academia){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Academia</li>";
+  }
+  if(beneficios.piscina){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Piscina</li>";
+  }
+  if(beneficios.wifi){
+    beneficio += "<li><iconify-icon icon='mdi-light:check'></iconify-icon>Wifi</li>";
+  }
+  return beneficio;
+
+  
+  
+}
