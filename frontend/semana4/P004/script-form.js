f0105548e206c577e960f34e28ff56f805aa706d
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-07 00:24:34 -0300

    feat: função de criar o objeto card

--- frontend/semana4/P004/script-form.js
+++ frontend/semana4/P004/script-form.js
@@ -1,3 +1,30 @@
+const form = document.querySelector("form");
+// carregarCardsDoStorage();
+form.addEventListener("submit", adicionaCard);
+
+function adicionaCard(event) {
+  event.preventDefault();
+  const dados = {
+    url: form.url.value,
+    titulo: form.titulo.value,
+    beneficios: {
+        cafe: form.cafe.checked,
+        ida_e_volta: form.ida_e_volta.checked,
+        almoco: form.almoco.checked,
+        academia: form.academia.checked,
+        piscina: form.piscina.checked,
+        wifi: form.wifi.checked,
+    },
+    diarias: form.diarias.value,
+    preco: form.preco.value,
+    parcelas: form.parcelas.value,
+    taxaInclusa: form.taxaInclusa.value,
+  };
+  let cardVar = new Card(dados.url, dados.titulo, dados.beneficios,dados.diarias,dados.preco,dados.parcelas,dados.taxaInclusa);
+  adicionaCardNoStorage(cardVar);
+  window.location.reload();
+}
+
 class Card {
   constructor(url, titulo, beneficios,diarias,preco,parcelas,taxaInclusa) {
     this.url = url;
