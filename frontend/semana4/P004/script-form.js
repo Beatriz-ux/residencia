2a5f1ca987b28eae96d58946449348ff519b7242
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-07 00:23:03 -0300

    feat: classe Card e o construtor

---
+++ frontend/semana4/P004/script-form.js
@@ -0,0 +1,11 @@
+class Card {
+  constructor(url, titulo, beneficios,diarias,preco,parcelas,taxaInclusa) {
+    this.url = url;
+    this.titulo = titulo;
+    this.beneficios = beneficios;
+    this.diarias = diarias;
+    this.preco = preco;
+    this.parcelas = parcelas;
+    this.taxaInclusa = taxaInclusa;
+  }
+}
