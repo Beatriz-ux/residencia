513d47ae183672ac41bc4dbcd45f352c858f9bd1
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-14 17:45:35 -0300

    feat: adicionando camadas

---
+++ Domain/TodoDb.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
