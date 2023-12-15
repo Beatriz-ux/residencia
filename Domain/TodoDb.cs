8ec11e3ea6e76526904c4687ca21f3d24bc80e29
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-15 17:27:41 -0300

    limpando branch

--- Domain/TodoDb.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Domain;
-public class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
-
-    public DbSet<Todo> Todos => Set<Todo>();
-}
