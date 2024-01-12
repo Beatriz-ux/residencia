bee801775b2f3bee6b74c7bfc20e0fa56372cf27
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-12 16:01:14 -0300

    feat: camada de infrastrutura com as conexões com o banco

---
+++ ProjetoCamadas/Infrastructure/TecheMedContext.cs
@@ -0,0 +1,44 @@
+﻿namespace Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using TechMedDomain;
+public class TecheMedContext : DbContext
+{
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+
+    public DbSet<Atendimento> Atendimentos { get; set; }
+
+    public DbSet<Exame> Exames { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var connectionString = "server=localhost;user=dotnet;password=residencia2023;database=techmed";
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Medico>().ToTable("Medicos").HasKey(m => m.Id);
+        modelBuilder.Entity<Paciente>().ToTable("Pacientes").HasKey(p => p.Id);
+        modelBuilder.Entity<Atendimento>().ToTable("Atendimentos").HasKey(a => a.AtendimentoId);
+        modelBuilder.Entity<Exame>().ToTable("Exames").HasKey(e => e.Id);
+
+        modelBuilder.Entity<Atendimento>()
+            .HasOne(a => a.Medico)
+            .WithMany(m => m.Atendimentos)
+            .HasForeignKey(a => a.MedicoId);
+
+        modelBuilder.Entity<Atendimento>()
+            .HasOne(a => a.Paciente)
+            .WithMany(p => p.Atendimentos)
+            .HasForeignKey(a => a.PacienteId);
+
+        modelBuilder.Entity<Exame>()
+            .HasOne(e => e.Atendimento)
+            .WithMany(a => a.Exames)
+            .HasForeignKey(e => e.AtendimentoId);
+    }
+}
